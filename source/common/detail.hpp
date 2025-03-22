d261c44f5e38123ccf188b019dad0f939983ef9b
XI-xi-Sakura <1959701844@qq.com>
2025-03-22 13:42:14 +0800

    common--detail.hpp--序列化和反序列化

--- source/common/detail.hpp
+++ source/common/detail.hpp
@@ -1,13 +1,29 @@
+/*
+    实现项目中用到的一些琐碎功能代码
+    * 日志宏的定义
+    * json的序列化和反序列化
+    * uuid的生成
+*/
 #pragma once
-#include <stdio.h>
-#include <time.h>
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <random>
+#include <sstream>
+#include <atomic>
+#include <iomanip>
+#include <jsoncpp/json/json.h>
+
+namespace bitrpc
+{
 #define LDBG 0
 #define LINF 1
 #define LERR 2
-#define LDEFAULT LINF
 
+#define LDEFAULT LDBG
 
-//日志宏定义
 #define LOG(level, format, ...)                                                                       \
     {                                                                                                 \
         if (level >= LDEFAULT)                                                                        \
@@ -22,3 +38,93 @@
 #define DLOG(format, ...) LOG(LDBG, format, ##__VA_ARGS__);
 #define ILOG(format, ...) LOG(LINF, format, ##__VA_ARGS__);
 #define ELOG(format, ...) LOG(LERR, format, ##__VA_ARGS__);
+
+    class JSON
+    {
+    public:
+        // 输⼊输出型参数
+        // root输⼊参数：表⽰要序列化的json对象
+        // body输出参数: 表⽰序列化之后的字符串
+
+        // class JSON_API StreamWriter {
+        //     virtual int write(Value const& root, std::ostream* sout) = 0;
+        //    }
+        //  class JSON_API StreamWriterBuilder : public StreamWriter::Factory {
+        //     virtual StreamWriter* newStreamWriter() const;
+        //    }
+        static bool serialize(const Json::Value &val, std::string &body) // 序列化
+        {
+            std::stringstream ss;
+            // 先实例化一个工厂类对象
+            Json::StreamWriterBuilder swb;
+            // 通过工厂类对象来生产派生类对象
+            std::unique_ptr<Json::StreamWriter> sw(swb.newStreamWriter());
+            int ret = sw->write(val, &ss);
+            if (ret != 0)
+            {
+                ELOG("json serialize failed!");
+                return false;
+            }
+            body = ss.str();
+            return true;
+        }
+        // 反序列化: 字符串 ->Json对象
+        // 输⼊输出型参数
+        // body输⼊参数: 表⽰需要反序列化的字符串
+        // val输出参数：表⽰反序列化后的json对象
+
+        // class JSON_API CharReader {
+        //     virtual bool parse(char const* beginDoc, char const* endDoc, Value* root, std::string* errs) = 0;
+        // }
+        
+        //  class JSON_API CharReaderBuilder : public CharReader::Factory {
+        //     virtual CharReader* newCharReader() const;
+        //  }
+        static bool unserialize(const std::string &body, Json::Value &val) // 反序列化
+        {
+            Json::CharReaderBuilder crb;
+            std::string errs;
+            std::unique_ptr<Json::CharReader> cr(crb.newCharReader());
+            bool ret = cr->parse(body.c_str(), body.c_str() + body.size(), &val, &errs);
+            if (ret == false)
+            {
+                ELOG("json unserialize failed : %s", errs.c_str());
+                return false;
+            }
+            return true;
+        }
+    };
+
+    class UUID
+    {
+    public:
+        static std::string uuid()
+        {
+            std::stringstream ss;
+            // 1. 构造一个机器随机数对象
+            std::random_device rd;
+            // 2. 以机器随机数为种子构造伪随机数对象
+            std::mt19937 generator(rd());
+            // 3. 构造限定数据范围的对象
+            std::uniform_int_distribution<int> distribution(0, 255);
+            // 4. 生成8个随机数，按照特定格式组织成为16进制数字字符的字符串
+            for (int i = 0; i < 8; i++)
+            {
+                if (i == 4 || i == 6)
+                    ss << "-";
+                ss << std::setw(2) << std::setfill('0') << std::hex << distribution(generator);
+            }
+            ss << "-";
+            // 5. 定义一个8字节序号，逐字节组织成为16进制数字字符的字符串
+            static std::atomic<size_t> seq(1); //  00 00 00 00 00 00 00 01
+            size_t cur = seq.fetch_add(1);
+            for (int i = 7; i >= 0; i--)
+            {
+                if (i == 5)
+                    ss << "-";
+                ss << std::setw(2) << std::setfill('0') << std::hex << ((cur >> (i * 8)) & 0xFF);
+            }
+            return ss.str();
+        }
+    };
+}
