f52b6197ae7830a3155ed58d9835d75c4d79c334
XI-xi-Sakura <1959701844@qq.com>
2025-03-22 16:20:05 +0800

    信息定义

--- source/common/detail.hpp
+++ source/common/detail.hpp
@@ -76,7 +76,7 @@ namespace bitrpc
         // class JSON_API CharReader {
         //     virtual bool parse(char const* beginDoc, char const* endDoc, Value* root, std::string* errs) = 0;
         // }
-        
+
         //  class JSON_API CharReaderBuilder : public CharReader::Factory {
         //     virtual CharReader* newCharReader() const;
         //  }
@@ -95,16 +95,22 @@ namespace bitrpc
         }
     };
 
+    // UUID(Universally Unique Identifier), 也叫通⽤唯一识别码，通常由32位16进制数字字符组成。
+    // UUID的标准型式包含32个16进制数字字符，以连字号分为五段，形式为8-4-4-4-12的32个字符，
+    // 如：550e8400-e29b-41d4-a716-446655440000。
+    // 在这里，uuid⽣成，我们采⽤⽣成8个随机数字，加上8字节序号，共16字节数组⽣成32位16进制字符的组合形式
+    // 来确保全局唯一的同时能够根据序号来分辨数据
     class UUID
     {
     public:
         static std::string uuid()
         {
             std::stringstream ss;
+
             // 1. 构造一个机器随机数对象
-            std::random_device rd;
+            std::random_device rd; // 基于硬件设备（如操作系统的随机数生成器）来生成随机数，因此生成的随机数具有较高的随机性和不可预测性。
             // 2. 以机器随机数为种子构造伪随机数对象
-            std::mt19937 generator(rd());
+            std::mt19937 generator(rd()); // 创建一个基于梅森旋转算法（Mersenne Twister algorithm）的伪随机数生成器对象
             // 3. 构造限定数据范围的对象
             std::uniform_int_distribution<int> distribution(0, 255);
             // 4. 生成8个随机数，按照特定格式组织成为16进制数字字符的字符串
@@ -113,11 +119,13 @@ namespace bitrpc
                 if (i == 4 || i == 6)
                     ss << "-";
                 ss << std::setw(2) << std::setfill('0') << std::hex << distribution(generator);
+                // 设置下一个输出项的最小字段宽度为 2,不足补0；并将其转换至16进制；
             }
             ss << "-";
             // 5. 定义一个8字节序号，逐字节组织成为16进制数字字符的字符串
-            static std::atomic<size_t> seq(1); //  00 00 00 00 00 00 00 01
-            size_t cur = seq.fetch_add(1);
+            static std::atomic<size_t> seq(1); //  00 00 00 00 00 00 00 01 //8字节
+            // 可以确保对变量的读写操作是原子的，即在多线程环境下不会出现数据竞争的问题。
+            size_t cur = seq.fetch_add(1); //+1操作
             for (int i = 7; i >= 0; i--)
             {
                 if (i == 5)
